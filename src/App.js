f6ec526d5fd64f602590c658b4970dfe84eb1200
leen <leenodeh287@gmail.com>
2023-11-23 20:42:29 +0300

    doneeeeeeeeeeeeeee finalllllllllly :)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './components/footer/Footer';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Cart from './pages/cart/Cart';
-import Products from './pages/products/Products';
+// import Products from './pages/products/Products';
 import SingleProduct from './pages/single-product/SingleProduct';
 import SpecialOffersPage from './pages/special-offers-page/SpecialOffersPage';
 
@@ -15,7 +15,7 @@ function App() {
       <Header />
          <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/products' element={<Products />} />
+            {/* <Route path='/products' element={<Products />} /> */}
             <Route path='/products/:id' element={<SingleProduct />} />
             <Route path='/cart' element={<Cart />} />
             <Route path='/special-offers/:id' element={<SpecialOffersPage />} />
