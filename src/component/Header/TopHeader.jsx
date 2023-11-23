6500af85463b2bd78d77fa7f1b864fbc4a6e630b
leen <leenodeh287@gmail.com>
2023-11-23 13:26:01 +0300

    add structure Header

---
+++ src/component/Header/TopHeader.jsx
@@ -0,0 +1,30 @@
+
+
+const TopHeader = () => {
+  return (
+    <div className="flex justify-between items-stretch p-3 bg-gray-600	">
+
+
+      <div >
+        <i className="bi bi-basket"></i>
+        نون
+      </div>
+
+
+      <div>أهلا وسهلا بكم</div>
+
+
+
+      <div>
+        <b>123-456-789</b>
+        <i className="bi bi-telephone"></i>
+        
+      </div>
+
+
+
+    </div>
+  );
+};
+
+export default TopHeader;
